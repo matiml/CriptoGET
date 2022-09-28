981aba9ef78e9e0fc1989070affd7a74f32404ca
matias ledesma <matimanu09@gmail.com>
2022-09-28 15:23:42 -0300

    correcciones form

--- script.js
+++ script.js
@@ -167,59 +167,29 @@ $(document).ready(function () {
     });
   });
 
-//   //ajax form
-//   $(function () {
-//     // Get the form.
-//     var form = $("#ajax-contact");
 
-//     // Get the messages div.
-//     var formMessages = $("#form-messages");
-
-//     // Set up an event listener for the contact form.
-//     $(form).submit(function (e) {
-//       // Stop the browser from submitting the form.
-//       e.preventDefault();
-
-//       // Serialize the form data.
-//       var formData = $(form).serialize();
-
-//       // Submit the form using AJAX.
-//       $.ajax({
-//         type: "POST",
-//         url: $(form).attr("action"),
-//         data: formData
-//       })
-//         .done(function (response) {
-//           // Make sure that the formMessages div has the 'success' class.
-//           $(formMessages).removeClass("error");
-//           $(formMessages).addClass("success");
-
-//           // Set the message text.
-//           $(formMessages).text(response);
-
-//           // Clear the form.
-//           $("#name").val("");
-//           $("#email").val("");
-//           $("#message").val("");
-//         })
-//         .fail(function (data) {
-//           // Make sure that the formMessages div has the 'error' class.
-//           $(formMessages).removeClass("success");
-//           $(formMessages).addClass("error");
-
-//           // Set the message text.
-//           if (data.responseText !== "") {
-//             $(formMessages).text(data.responseText);
-//           } else {
-//             $(formMessages).text(
-//               "Oops! An error occured and your message could not be sent."
-//             );
-//           }
-//         });
-//     });
-//   });
 });
 
 
 
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_su88n8v';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
